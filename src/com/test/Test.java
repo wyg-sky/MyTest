a95bec5ac3db803a4c5c255e8a9f1f69224d2d17
wyg-sky <375244737@qq.com>
2016-12-20 22:29:50 +0800

    测试项目提交

---
+++ src/com/test/Test.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class Test {
+
+    /**
+     * @description:测试用例
+     * @param args
+     * @author WangYG
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String str = "abc";
+        System.out.println("..........."+str);
+        System.out.println("..........."+str);
+        System.out.println("..........."+str);
+    }
+
+}
