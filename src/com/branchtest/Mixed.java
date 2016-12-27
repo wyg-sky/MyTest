b98db40a750c1fdda0c599b907c82e5893d81a4d
wyg-sky <375244737@qq.com>
2016-12-27 15:26:00 +0800

    Mixed Test

---
+++ src/com/branchtest/Mixed.java
@@ -0,0 +1,9 @@
+package com.branchtest;
+
+public class Mixed {
+
+    public static void main(String[] args) {
+        System.out.println("重置功能之Mixed：Mixed - 当前分支重置到指定commit记录位置，索引被更新，工作树不变；");
+    }
+
+}
