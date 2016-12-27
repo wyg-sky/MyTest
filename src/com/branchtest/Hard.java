86d82217d567350fe940959098909c4ba0088324
wyg-sky <375244737@qq.com>
2016-12-27 15:28:04 +0800

    Hard Test

---
+++ src/com/branchtest/Hard.java
@@ -0,0 +1,9 @@
+package com.branchtest;
+
+public class Hard {
+
+    public static void main(String[] args) {
+        System.out.println("重置功能之Hard：Hard - 当前分支重置到指定commit记录位置，索引和工作树都更新.");
+    }
+
+}
