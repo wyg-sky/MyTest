74dc7898e837b3eaef82d135af20d50996e0f745
wyg-sky <375244737@qq.com>
2016-12-27 10:29:10 +0800

    develop分之下操作： 1.修改BranchTest.java文件。增加develop输出语句。 2.新增Develop.java文件。

---
+++ src/com/branchtest/Develop.java
@@ -0,0 +1,40 @@
+package com.branchtest;
+
+public class Develop {
+    private String projectName;
+    private Integer projectCycle;
+    private Integer projectPeople;
+    private Double projectFunds;
+    private String projectDescription;
+    public String getProjectName() {
+        return projectName;
+    }
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+    public Integer getProjectCycle() {
+        return projectCycle;
+    }
+    public void setProjectCycle(Integer projectCycle) {
+        this.projectCycle = projectCycle;
+    }
+    public Integer getProjectPeople() {
+        return projectPeople;
+    }
+    public void setProjectPeople(Integer projectPeople) {
+        this.projectPeople = projectPeople;
+    }
+    public Double getProjectFunds() {
+        return projectFunds;
+    }
+    public void setProjectFunds(Double projectFunds) {
+        this.projectFunds = projectFunds;
+    }
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+    public void setProjectDescription(String projectDescription) {
+        this.projectDescription = projectDescription;
+    }
+
+}
