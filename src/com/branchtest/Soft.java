edc8d5de1fb679b3d3ce8b2dbbbf435353c73f33
wyg-sky <375244737@qq.com>
2016-12-27 14:50:52 +0800

    测试重置功能Soft

---
+++ src/com/branchtest/Soft.java
@@ -0,0 +1,9 @@
+package com.branchtest;
+
+public class Soft {
+
+    public static void main(String[] args) {
+        System.out.println("重置功能之Soft：Soft - 当前分支重置到指定commit记录位置，索引和工作树不变；");
+    }
+
+}
